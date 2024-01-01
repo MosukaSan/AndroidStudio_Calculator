c34068927193ed1731fb14ccaf1a1c9a3a5eedc1
Lucas <mosukasangamer@gmail.com>
2023-12-31 21:23:06 -0300

    operators

--- app/src/main/java/com/devlucaslima/calculator/MainActivity.kt
+++ app/src/main/java/com/devlucaslima/calculator/MainActivity.kt
@@ -2,110 +2,127 @@ package com.devlucaslima.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.view.Display
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.widget.doOnTextChanged
+import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var btn0 = findViewById<Button>(R.id.btn0)
-        var btn1 = findViewById<Button>(R.id.btn1)
-        var btn2 = findViewById<Button>(R.id.btn2)
-        var btn3 = findViewById<Button>(R.id.btn3)
-        var btn4 = findViewById<Button>(R.id.btn4)
-        var btn5 = findViewById<Button>(R.id.btn5)
-        var btn6 = findViewById<Button>(R.id.btn6)
-        var btn7 = findViewById<Button>(R.id.btn7)
-        var btn8 = findViewById<Button>(R.id.btn8)
-        var btn9 = findViewById<Button>(R.id.btn9)
-        var btnDot = findViewById<Button>(R.id.btnDot)
-        var btnBackspace = findViewById<Button>(R.id.btnBackspace)
-        var Display1 = findViewById<TextView>(R.id.txtDisplay)
-        var Display1Editable = Display1.text as Editable
+        //Botões
+        val btn0 = findViewById<Button>(R.id.btn0)
+        val btn1 = findViewById<Button>(R.id.btn1)
+        val btn2 = findViewById<Button>(R.id.btn2)
+        val btn3 = findViewById<Button>(R.id.btn3)
+        val btn4 = findViewById<Button>(R.id.btn4)
+        val btn5 = findViewById<Button>(R.id.btn5)
+        val btn6 = findViewById<Button>(R.id.btn6)
+        val btn7 = findViewById<Button>(R.id.btn7)
+        val btn8 = findViewById<Button>(R.id.btn8)
+        val btn9 = findViewById<Button>(R.id.btn9)
+        val btnDot = findViewById<Button>(R.id.btnDot)
+        val btnBackspace = findViewById<Button>(R.id.btnBackspace)
+        val btnPlus = findViewById<Button>(R.id.btnPlus)
+        val btnMinus = findViewById<Button>(R.id.btnMinus)
+        val btnTimes = findViewById<Button>(R.id.btnTimes)
+        val btnDivision = findViewById<Button>(R.id.btnDivision)
+        val btnPower = findViewById<Button>(R.id.btnPower)
+        val btnAC = findViewById<Button>(R.id.btnAC)
+        val btnEqual = findViewById<Button>(R.id.btnEqual)
+        val display1 = findViewById<TextView>(R.id.txtDisplay)
+        val displayResultado = findViewById<TextView>(R.id.txtResultado)
 
         btn0.setOnClickListener{
-            if(Display1.text == "0"){
-                Display1.text = "0"
-            } else {
-                Display1.append("0")
-            }
+            val display1String = display1.text.toString()
+            display1.text = display1String + "0"
         }
         btn1.setOnClickListener{
-            if(Display1.text == "0"){
-                Display1.text = "1"
-            } else {
-                Display1.append("1")
-            }
+            val display1String = display1.text.toString()
+            display1.text = display1String + "1"
         }
         btn2.setOnClickListener{
-            if(Display1.text == "0"){
-                Display1.text = "2"
-            } else {
-                Display1.append("2")
-            }
+            val display1String = display1.text.toString()
+            display1.text = display1String + "2"
         }
         btn3.setOnClickListener{
-            if(Display1.text == "0"){
-                Display1.text = "3"
-            } else {
-                Display1.append("3")
-            }
+            val display1String = display1.text.toString()
+            display1.text = display1String + "3"
         }
         btn4.setOnClickListener{
-            if(Display1.text == "0"){
-                Display1.text = "4"
-            } else {
-                Display1.append("4")
-            }
+            val display1String = display1.text.toString()
+            display1.text = display1String + "4"
         }
         btn5.setOnClickListener{
-            if(Display1.text == "0"){
-                Display1.text = "5"
-            } else {
-                Display1.append("5")
-            }
+            val display1String = display1.text.toString()
+            display1.text = display1String + "5"
         }
         btn6.setOnClickListener{
-            if(Display1.text == "0"){
-                Display1.text = "6"
-            } else {
-                Display1.append("6")
-            }
+            val display1String = display1.text.toString()
+            display1.text = display1String + "6"
         }
         btn7.setOnClickListener{
-            if(Display1.text == "0"){
-                Display1.text = "7"
-            } else {
-                Display1.append("7")
-            }
+            val display1String = display1.text.toString()
+            display1.text = display1String + "7"
         }
         btn8.setOnClickListener{
-            if(Display1.text == "0"){
-                Display1.text = "8"
-            } else {
-                Display1.append("8")
-            }
+            val display1String = display1.text.toString()
+            display1.text = display1String + "8"
         }
         btn9.setOnClickListener{
-            if(Display1.text == "0"){
-                Display1.text = "9"
-            } else {
-                Display1.append("9")
-            }
+            val display1String = display1.text.toString()
+            display1.text = display1String + "9"
         }
         btnDot.setOnClickListener{
-            if(Display1.text != "0"){
-            Display1.append(".")
-            }
+
+        }
+        btnPlus.setOnClickListener{
+            val display1String = display1.text.toString()
+            display1.text = display1String + "+"
+        }
+        btnMinus.setOnClickListener{
+            val display1String = display1.text.toString()
+            display1.text = display1String + "-"
+        }
+        btnTimes.setOnClickListener{
+            val display1String = display1.text.toString()
+            display1.text = display1String + "*"
+        }
+        btnDivision.setOnClickListener{
+            val display1String = display1.text.toString()
+            display1.text = display1String + "/"
+        }
+        btnPower.setOnClickListener{
+            val display1String = display1.text.toString()
+            display1.text = display1String + "^"
+        }
+        btnAC.setOnClickListener{
+            display1.text = ""
         }
         btnBackspace.setOnClickListener{
-            if(Display1.text != "0" || Display1.length() != 1){
-                Display1Editable.delete(Display1Editable.length - 1, Display1Editable.length)
+            val display1String = display1.text.toString()
+            if (display1String.isNotEmpty()) {
+                val display1Backspace = display1String.substring(0, display1String.length - 1)
+                display1.text = display1Backspace
+            }
+        }
+        btnEqual.setOnClickListener {
+            val expressao = display1.text.toString()
+
+            try {
+                val result = ExpressionBuilder(expressao).build().evaluate()
+                val formattedResult = if (result % 1 == 0.0) {
+                    result.toInt().toString()
+                } else {
+                    result.toString()
+                }
+                display1.text = formattedResult
+            } catch (e: Exception) {
+                display1.text = "Error"
             }
         }
     }
 }
+
